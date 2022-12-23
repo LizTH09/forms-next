e269d76dd102749a084655ecb94efb1d13f7733c
Lizbeth Selena Aldana Tarifeño <lsat-1699@hotmail.com>
2022-12-23 00:57:51 -0500

    añadiendo estilo content 2

--- components/forms/pdf/PdfAmendment.js
+++ components/forms/pdf/PdfAmendment.js
@@ -33,10 +33,9 @@ const styles = StyleSheet.create({
     fontFamily: "Montserrat",
   },
   content: {
-    width: "90%",
+    width: "90vw",
     display: "flex",
     flexDirection: "column",
-    position: "relative",
   },
 });
 const PdfAmendment = ({ text, form, date }) => {
