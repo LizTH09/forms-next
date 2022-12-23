494afa03173908f9c29b29a0b3566aa29fe528a2
Lizbeth Selena Aldana Tarifeño <lsat-1699@hotmail.com>
2022-12-22 22:46:45 -0500

    probando insertar valores de variables con estilos

--- components/forms/pdf/PdfAmendment.js
+++ components/forms/pdf/PdfAmendment.js
@@ -22,6 +22,12 @@ const PdfAmendment = ({ text, form, date }) => {
       flexDirection: "column",
       position: "relative",
     },
+    body: {
+      marginLeft: 61,
+      marginRight: 80,
+      marginVertical: 15,
+      marginBottom: 40,
+    },
     title: {
       position: "absolute",
       left: 0,
@@ -34,11 +40,40 @@ const PdfAmendment = ({ text, form, date }) => {
       display: "flex",
       flexWrap: "wrap",
     },
-    body: {
-      marginLeft: 61,
-      marginRight: 80,
-      marginVertical: 15,
-      marginBottom: 40,
+    line: {
+      width: "100%",
+      height: 1,
+      backgroundColor: "#e1e2e2d3",
+      marginBottom: 20,
+    },
+    contentStep: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "20px",
+      justifyContent: "space-between",
+      margin: 5,
+      marginBottom: 20,
+      fontSize: 12,
+    },
+    twoColumn: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    column: {
+      display: "flex",
+      flexDirection: "column",
+      width: "530px",
+      gap: "8px",
+    },
+    inputLabel: {
+      fontWeight: "semibold",
+      paddingVertical: 5,
+      width: "100%",
+    },
+    inputValue: {
+      paddingTop: 5,
+      paddingBottom: 10,
     },
   });
   console.log("on ta el pdf");
@@ -51,6 +86,28 @@ const PdfAmendment = ({ text, form, date }) => {
           <View style={styles.body}>
             <View style={styles.title}>
               <Text>{text.steps.step1.title}</Text>
+            </View>
+            <Text style={styles.subTitle}>{text.steps.step1.title}</Text>
+            <View style={styles.line}></View>
+            <View style={styles.contentStep}>
+              <View style={styles.twoColumn}>
+                <View style={styles.column}>
+                  <Text style={styles.inputLabel}>
+                    {text.steps.step1.labels.label1} :{" "}
+                  </Text>
+                  <Text style={styles.inputValue}>
+                    {form.company_information__company_name}
+                  </Text>
+                </View>
+                <View style={styles.column}>
+                  <Text style={styles.inputLabel}>
+                    {text.steps.step1.labels.label2} :{" "}
+                  </Text>
+                  <Text style={styles.inputValue}>
+                    {form.company_information__tax_id}
+                  </Text>
+                </View>
+              </View>
             </View>
           </View>
         </View>
