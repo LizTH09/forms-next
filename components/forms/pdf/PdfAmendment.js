c697bbe1419956a0eeb9aea9cb14f7bde3d1c14f
Lizbeth Selena Aldana Tarifeño <lsat-1699@hotmail.com>
2022-12-23 13:56:27 -0500

    probando input 1 y 2

--- components/forms/pdf/PdfAmendment.js
+++ components/forms/pdf/PdfAmendment.js
@@ -34,6 +34,7 @@ const PdfAmendment = ({ text, form, date }) => {
       display: "flex",
       flexDirection: "column",
       position: "relative",
+      fontFamily: "Montserrat",
     },
     marginDecoration: {
       width: "7vw",
@@ -71,18 +72,55 @@ const PdfAmendment = ({ text, form, date }) => {
       display: "flex",
       justifyContent: "center",
     },
+    body: {
+      position: "absolute",
+      height: "70vh",
+      width: "80vw",
+      top: "17vh",
+      left: "9vw",
+    },
     subTitle: {
       fontSize: 16,
       fontWeight: "semibold",
       color: "#2CAF95",
     },
-    body: {
-      position: "absolute",
-      backgroundColor: "red",
-      height: "70vh",
-      width: "80vw",
-      top: "15vh",
-      left: "9vw",
+    line: {
+      width: "100%",
+      height: 1,
+      backgroundColor: "#e1e2e2d3",
+      marginBottom: 20,
+    },
+    contentStep: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "20px",
+      justifyContent: "space-between",
+      margin: 5,
+      marginBottom: 20,
+      fontSize: 12,
+    },
+    twoColumn: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    column: {
+      display: "flex",
+      flexDirection: "column",
+      width: "530px",
+      gap: "8px",
+    },
+    inputLabel: {
+      fontWeight: "semibold",
+      fontSize: 13,
+      paddingVertical: 5,
+      width: "100%",
+    },
+    inputValue: {
+      paddingLeft: 10,
+      paddingTop: 5,
+      paddingBottom: 10,
+      fontSize: 11,
     },
   });
   return (
@@ -104,6 +142,25 @@ const PdfAmendment = ({ text, form, date }) => {
           </View>
           <View style={stylesPdf.body}>
             <Text style={stylesPdf.subTitle}>{text.steps.step1.title}</Text>
+            <View style={stylesPdf.line}></View>
+            <View style={stylesPdf.twoColumn}>
+              <View style={stylesPdf.column}>
+                <Text style={stylesPdf.inputLabel}>
+                  {text.steps.step1.labels.label1} :{" "}
+                </Text>
+                <Text style={stylesPdf.inputValue}>
+                  {form.company_information__company_name}
+                </Text>
+              </View>
+              <View style={stylesPdf.column}>
+                <Text style={stylesPdf.inputLabel}>
+                  {text.steps.step1.labels.label2} :{" "}
+                </Text>
+                <Text style={stylesPdf.inputValue}>
+                  {form.company_information__tax_id}
+                </Text>
+              </View>
+            </View>
           </View>
         </View>
       </Page>
